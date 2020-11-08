ecf832529ab8550d666c49e0eae6543508e639c1
Gowtham R <43744711+gowthamr1999@users.noreply.github.com>
2020-11-08 16:27:46 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+
+        <footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <p class="grey-text text-lighten-4">For More about my activities see through the links  provided
+                	<br>
+                	<br>
+                	<br>
+                Phone number : <span class="blue-text text-lighten-3">+91 9611567198</span>
+                <br>
+                Email : <span class="blue-text text-lighten-3">gowthamreddy19999@gmail </span> 
+            </p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text"></h5>
+                <ul>
+                  <li class="active"><a class="grey-text text-lighten-3" href="https://twitter.com/Gowtham_R_">Twitter</a></li>
+                  <li class="active"><a class="grey-text text-lighten-3" href="https://www.facebook.com/gowtham.reddy.1222/">Facebook</a></li>
+                  <li class="active"><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/gowtham-r-3a6990173/">LinkedIn</a></li>
+                  <li class="active"><a class="grey-text text-lighten-3" href="https://github.com/gowthamr1999?tab=repositories">GitHub</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2020 Copyright
+            </div>
+          </div>
+        </footer>
+</body>
---
+++ header.php
@@ -0,0 +1,39 @@
+<head>
+ 	<title>GOWTHAM R</title>
+ 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+ 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ 	<style type="text/css">
+ 		.brand{
+ 			background: red;
+ 		}
+ 		.brand-text{
+ 			background: red;
+ 		}
+ 		.brand-logo{
+ 			color: red;
+ 		}
+ 		.card{
+ 			color: red;
+ 			position: relative;
+ 		}
+ 		.card-content{
+ 			font-weight: bolder;
+ 			color: black;
+ 		}
+
+ 	
+ 	</style>
+ </head>
+ <body class="grey lighten-4">
+ 	<div class="nabbar-fixed">
+ 	<nav class="black nav-wrapper z-depth-0">
+ 		<div class="container">
+ 			<a href="index.php" class="brand-logo left">GOWTHAM R</a>
+ 			<ul id="nav-mobile" class="right hide-on-small and down">
+ 				<li class="active"><a href="about.php" class="btn brand z-depth-0 waves-effect waves-teal btn-flat">ABOUT</a></li>
+ 				<li class="active"><a href="project.php" class="btn brand z-depth-0 waves-effect waves-teal btn-flat">Projects</a></li>
+ 				<li class="active"><a href="contact.php" class="btn brand z-depth-0 waves-effect waves-teal btn-flat">Contact Me</a></li>
+ 			</ul>
+ 		</div>
+ 	</nav>
+</div>
